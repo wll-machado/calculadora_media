ba03cef443f6541bc84a38fac94d4620e10b8a29
Wellington <w9llington@gmail.com>
2023-12-13 20:07:51 -0300

    calculando media

--- script.js
+++ script.js
@@ -3,11 +3,27 @@ var linhas = '';
 
 const imgAprovado = '<img src="./images/aprovado.png" alt"aprovado"/>'
 const imgReprovado = '<img src="./images/reprovado.png" alt"reprovado"/>'
+const atividades = [];
+const notas = [];
+const aprovado = '<span class="aprovado">Aprovado</span>';
+const reprovado = '<span class="reprovado">Reprovado</span>';
+
 form.addEventListener('submit', function(e) {
     e.preventDefault();
 
+    adicionarLinha();
+    atualizarTabela();
+    mediaFinal();
+    exibirMedia()
+})
+
+function adicionarLinha() {
+    
     var inputNomeAtividade = document.getElementById('nome-atividade');
     var inputNotaAtividade = document.getElementById('nota-atividade');
+
+    atividades.push(inputNomeAtividade.value);
+    notas.push(parseFloat(inputNotaAtividade.value));
 
     let linha = `<tr>`;
     linha += `<td>${inputNomeAtividade.value}</td>`;
@@ -17,9 +33,29 @@ form.addEventListener('submit', function(e) {
 
     linhas += linha;
 
-    var corpoTabela = document.querySelector('tbody');
-    corpoTabela.innerHTML = linhas;
-
     inputNotaAtividade.value = '';
     inputNomeAtividade.value = '';
-})
+}
+
+function atualizarTabela(){
+    var corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+}
+
+function exibirMedia(){
+    var mediaResultadoFinal = mediaFinal();
+
+    document.getElementById('media-final').innerHTML = mediaResultadoFinal;
+    document.getElementById('media-resultado').innerHTML = mediaResultadoFinal >= 7 ? aprovado: reprovado; 
+}
+
+function mediaFinal(){
+    let somaDasNotas = 0;
+
+    for (let i = 0; i < notas.length; i++) {
+        somaDasNotas += notas[i];
+    }
+    
+    return somaDasNotas / notas.length;
+}
+
